7425bac0c066744f131c1bb21e45d2369ee02ae0
ocanath <ocanath@gmail.com>
2024-11-16 18:04:54 -0800

    hello world

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 #include "init.h"
-#include "PPP.h"
+#include "m_uart.h"
 
 
 
@@ -12,8 +12,18 @@ int main(void)
 	MX_DMA_Init();
 	MX_ADC1_Init();
 	MX_USART2_UART_Init();
+
+	uint32_t uart_tx_ts = 0;
 	while (1)
 	{
+		uint32_t tick = HAL_GetTick();
+
+		if( (tick - uart_tx_ts) > 15)
+		{
+			uart_tx_ts = tick;
+			m_uart_tx_start(&m_huart2, (uint8_t*)"Hello\n", 6);
+		}
+
 	}
 }
 
