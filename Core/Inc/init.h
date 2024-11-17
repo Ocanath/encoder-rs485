a5bbccafe09f65a14992bfbf3c9c1e844cc21c87
ocanath <ocanath@gmail.com>
2024-11-16 17:53:45 -0800

    init commit encoder project

---
+++ Core/Inc/init.h
@@ -0,0 +1,23 @@
+/*
+ * init.h
+ *
+ *  Created on: Nov 16, 2024
+ *      Author: ocanath
+ */
+
+#ifndef INC_INIT_H_
+#define INC_INIT_H_
+
+#include "main.h"
+
+extern ADC_HandleTypeDef hadc1;
+
+extern UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_ADC1_Init(void);
+void MX_USART2_UART_Init(void);
+
+
+#endif /* INC_INIT_H_ */
---
+++ Core/Src/main.c
@@ -0,0 +1,14 @@
+#include "init.h"
+
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_USART2_UART_Init();
+  while (1)
+  {
+  }
+}
