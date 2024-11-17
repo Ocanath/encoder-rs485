05b9585187d930f8cff617885a15a882a7806ed6
ocanath <ocanath@gmail.com>
2024-11-16 20:21:45 -0800

    added addressin response with dummy addresss

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,14 +1,34 @@
 #include "init.h"
 #include "m_uart.h"
+#include "PPP.h"
 
 #define NUM_ADC 2
 
+#define DUMMY_ADDRESS 0xAFFA
+
 uint16_t dma_adc_raw[NUM_ADC] = {0};
 
-static uint8_t received_data = 0;
+static uint8_t request_received = 0;
+
 void ppp_rx_cplt_callback(void)
 {
-	received_data = 1;
+	uint16_t * pbu16 = (uint16_t*)(&m_huart2.ppp_unstuff_buf[0]);
+	if(pbu16[0] == DUMMY_ADDRESS)
+	{
+		request_received = 1;
+	}
+}
+
+/*
+Generic hex checksum calculation.
+TODO: use this in the psyonic API
+ */
+uint16_t get_checksum16(uint16_t* arr, int size)
+{
+	int16_t checksum = 0;
+	for (int i = 0; i < size; i++)
+		checksum += (int16_t)arr[i];
+	return -checksum;
 }
 
 
@@ -21,25 +41,26 @@ int main(void)
 	MX_ADC1_Init();
 	MX_USART2_UART_Init();
 
-
-
-//	uint32_t uart_tx_ts = 0;
 	uint32_t led_ts = 0;
+
+	uint16_t uart_tx_buf[1+NUM_ADC+1] = {0};	//8 bytes. Two address, four payload, two checksum
+	uint8_t uart_stuff_buf[ sizeof(uart_tx_buf) * 2 + 2] = {0};
+
 	while (1)
 	{
 		uint32_t tick = HAL_GetTick();
 
 		HAL_ADC_Start_DMA(&hadc1, (uint32_t * )dma_adc_raw, NUM_ADC);
 
-//		if( (tick - uart_tx_ts) > 5)
-//		{
-//			uart_tx_ts = tick;
-//			m_uart_tx_start(&m_huart2, (uint8_t*)"Hello\r\n", 7);
-//		}
-		if(received_data != 0)
+		if(request_received != 0)
 		{
-			m_uart_tx_start(&m_huart2, (uint8_t*)"Hello\r\n", 7);
-			received_data = 0;
+			uart_tx_buf[0] = DUMMY_ADDRESS;
+			uart_tx_buf[1] = dma_adc_raw[0];
+			uart_tx_buf[2] = dma_adc_raw[1];
+			uart_tx_buf[3] = get_checksum16(uart_tx_buf, 3);
+			int num_bytes = PPP_stuff((uint8_t*)uart_tx_buf, sizeof(uart_tx_buf), uart_stuff_buf, sizeof(uart_stuff_buf));
+			m_uart_tx_start(&m_huart2, uart_stuff_buf, num_bytes);
+			request_received = 0;
 		}
 
 
