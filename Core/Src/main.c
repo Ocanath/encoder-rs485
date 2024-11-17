662b1f4022989e65f15e772731df1904fea629c4
ocanath <ocanath@gmail.com>
2024-11-16 19:54:36 -0800

    started with the DMA, doesn't work yet

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,9 +1,11 @@
 #include "init.h"
 #include "m_uart.h"
 
+#define NUM_ADC 2
+
+uint16_t dma_adc_raw[NUM_ADC] = {0};
 
 
-static uint32_t tick = 0;
 int main(void)
 {
 	HAL_Init();
@@ -13,11 +15,14 @@ int main(void)
 	MX_ADC1_Init();
 	MX_USART2_UART_Init();
 
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t * )dma_adc_raw, NUM_ADC);
+
+
 	uint32_t uart_tx_ts = 0;
 	uint32_t led_ts = 0;
 	while (1)
 	{
-		tick = HAL_GetTick();
+		uint32_t tick = HAL_GetTick();
 
 		if( (tick - uart_tx_ts) > 5)
 		{
