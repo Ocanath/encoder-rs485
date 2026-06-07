54bfc9fa968649b01f9200e3a9819d1f89362291
ocanath <ocanath@gmail.com>
2026-06-06 18:55:24 -0700

    delete uart stuff from main, delete uart file

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,35 +1,10 @@
 #include "init.h"
-#include "m_uart.h"
-#include "PPP.h"
 #include "fds.h"
 
 #define NUM_ADC 2
 
 uint16_t dma_adc_raw[NUM_ADC] = {0};
 
-static uint8_t request_received = 0;
-
-
-void ppp_rx_cplt_callback(void)
-{
-	uint16_t * pbu16 = (uint16_t*)(&m_huart2.ppp_unstuff_buf[0]);
-	if(pbu16[0] == fs_settings.address)
-	{
-		request_received = 1;
-	}
-}
-
-/*
-Generic hex checksum calculation.
-TODO: use this in the psyonic API
- */
-uint16_t get_checksum16(uint16_t* arr, int size)
-{
-	int16_t checksum = 0;
-	for (int i = 0; i < size; i++)
-		checksum += (int16_t)arr[i];
-	return -checksum;
-}
 
 /*
  * Helper function to load all the fds_mp params
@@ -53,7 +28,6 @@ int main(void)
 	MX_DMA_Init();
 	MX_ADC1_Init();
 	MX_USART2_UART_Init();
-	m_uart_enable_interrupt_flags(&m_huart2);
 	uint32_t led_ts = 0;
 
 	uint16_t uart_tx_buf[1+NUM_ADC+1] = {0};	//8 bytes. Two address, four payload, two checksum
@@ -65,16 +39,6 @@ int main(void)
 
 		HAL_ADC_Start_DMA(&hadc1, (uint32_t * )dma_adc_raw, NUM_ADC);
 
-		if(request_received != 0)
-		{
-			uart_tx_buf[0] = fs_settings.address;
-			uart_tx_buf[1] = dma_adc_raw[0];
-			uart_tx_buf[2] = dma_adc_raw[1];
-			uart_tx_buf[3] = get_checksum16(uart_tx_buf, 3);
-			int num_bytes = PPP_stuff((uint8_t*)uart_tx_buf, sizeof(uart_tx_buf), uart_stuff_buf, sizeof(uart_stuff_buf));
-			m_uart_tx_start(&m_huart2, uart_stuff_buf, num_bytes);
-			request_received = 0;
-		}
 
 		if(tick - led_ts > 333)
 		{
